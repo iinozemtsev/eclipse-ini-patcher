19ffb15f57eff8c6fdb90669ef0cde730d791e93
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2011-04-15 16:23:15 +0700

    Fixed issue #1'

--- src/main/java/com/xored/tycho/patcher/MyMojo.java
+++ src/main/java/com/xored/tycho/patcher/MyMojo.java
@@ -16,18 +16,15 @@ package com.xored.tycho.patcher;
  * limitations under the License.
  */
 
-import org.apache.maven.model.Plugin;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.FileUtils;
 
 /**
  * Goal which modifies eclipse.ini file for Mac OS X platform.
@@ -36,11 +33,6 @@ import java.util.List;
  * @phase package
  */
 public class MyMojo extends AbstractMojo {
-	private static final String ERROR_GET_CONFIG = "Can't get tycho-p2-director plugin configuration, execution stopped";
-	private static final String ERROR_READ_CONFIG = "Configuration of tycho-p2-director plugin is not understood, execution stopped";
-	private static final String ERROR_NO_PRODUCTS = "No products found in tycho-p2-director configuration, execution stopped";
-
-	private static final String ERROR_NO_PRODUCT = "Product %s is not found in target dir, skipped";
 	private static final String ERROR_NO_INI_FILE = "Product %s does not contain eclipse.ini for Mac OS X, skipped";
 	private static final String ERROR_NO_LAUNCHER = "Product %s does not contain equinox launcher, skipped";
 	private static final String ERROR_ALREADY_PATCHED = "Product %s already contains startup arg in eclipse.ini, skipped";
@@ -52,86 +44,78 @@ public class MyMojo extends AbstractMojo {
 	 */
 	private File outputDirectory;
 
-	/**
-	 * @parameter expression="${project}"
-	 * */
-	private MavenProject project;
-
 	public void execute() throws MojoExecutionException {
-		// get products configuration
-		Plugin plugin = project
-				.getPlugin("org.sonatype.tycho:tycho-p2-director-plugin");
-		if (plugin == null) {
-			getLog().warn(ERROR_GET_CONFIG);
-			return;
-		}
-
-		Object config = plugin.getConfiguration();
-		if (!(config instanceof Xpp3Dom)) {
-			getLog().warn(ERROR_READ_CONFIG);
-			return;
-		}
-
-		Xpp3Dom dom = (Xpp3Dom) plugin.getConfiguration();
-		Xpp3Dom products = dom.getChild("products");
-		if (products == null) {
-			getLog().warn(ERROR_READ_CONFIG);
-			return;
-		}
-
-		List<String> productIds = new ArrayList<String>();
-		for (Xpp3Dom product : products.getChildren("product")) {
-			Xpp3Dom productId = product.getChild("id");
-			if (productId != null) {
-				productIds.add(productId.getValue());
+		File productsDir = new File(outputDirectory, "products");
+		for(File productFile  : productsDir.listFiles()) {
+			if(productFile.isDirectory()) {
+				patchProduct(productFile);
 			}
-		}
-
-		if (productIds.isEmpty()) {
-			getLog().warn(ERROR_NO_PRODUCTS);
-		}
-
-		for (String productId : productIds) {
-			patchProduct(productId);
 		}
 	}
 
-	private void patchProduct(String productId) {
-		File dir = new File(new File(outputDirectory, "products"), productId);
-		if (!dir.exists() || !dir.isDirectory()) {
-			getLog().warn(String.format(ERROR_NO_PRODUCT, productId));
-			return;
-		}
-
+	private void patchProduct(File productDir) {
 		// Ideally we should use tycho project utils here to get
 		// destination directory of Mac OS X product, but for now just
 		// assume default location
 
-		File macDir = new File(dir, "macosx/cocoa/x86_64");
-		File iniFile = new File(macDir,
-				"Eclipse.app/Contents/MacOS/eclipse.ini");
-		File launcherFile = findLauncher(macDir);
-		if (!launcherFile.exists()) {
-			getLog().warn(String.format(ERROR_NO_LAUNCHER, productId));
+		File macDir = new File(productDir, "macosx/cocoa/x86_64");
+		if(!macDir.exists() || !macDir.isDirectory()) {
 			return;
 		}
+		
+		while(macDir.list().length == 1) {
+			macDir = macDir.listFiles()[0];
+		}
+		File launcherFile = findLauncher(macDir);
+		if (launcherFile == null || !launcherFile.exists()) {
+			getLog().warn(String.format(ERROR_NO_LAUNCHER, productDir.getName()));
+			return;
+		}
+		File launcherLib = findLauncherLib(macDir);
+		if (launcherFile == null || !launcherFile.exists()) {
+			getLog().warn(String.format(ERROR_NO_LAUNCHER, productDir.getName()));
+			return;
+		}
+
+
+		for (File app : getAppDirs(macDir)) {
+			File iniDir = new File(new File(app, "Contents"), "MacOS");
+			if (!iniDir.exists() || !iniDir.isDirectory()) {
+				continue;
+			}
+			for (File iniFile : getIniFiles(iniDir)) {
+				patchIniFile(productDir.getName(), launcherFile, launcherLib, iniFile);
+			}
+		}
+
+	}
+
+	private void patchIniFile(String productDirName, File launcherFile,
+			File launcherLib, File iniFile) {
 		if (!iniFile.exists()) {
-			getLog().warn(String.format(ERROR_NO_INI_FILE, productId));
+			getLog().warn(String.format(ERROR_NO_INI_FILE, productDirName));
 			return;
 		}
 
 		try {
 			String contents = FileUtils.fileRead(iniFile);
-			if (contents.contains(STARTUP_ARG)) {
-				getLog().warn(String.format(ERROR_ALREADY_PATCHED, productId));
-				return;
-			}
-			// prepend contents with location
 			StringBuilder patched = new StringBuilder();
-			patched.append(STARTUP_ARG).append("\n");
-			patched.append(
-					String.format("../../../plugins/%s", launcherFile.getName()))
-					.append("\n");
+			if (contents.contains(STARTUP_ARG)) {
+				getLog().warn(String.format(ERROR_ALREADY_PATCHED, productDirName));
+			} else {
+				patched.append(STARTUP_ARG).append("\n");
+				patched.append(
+						String.format("../../../plugins/%s",
+								launcherFile.getName())).append("\n");
+			}
+			if (contents.contains(LAUNCHER_LIB_ARG)) {
+				getLog().warn(String.format(ERROR_ALREADY_PATCHED, productDirName));
+			} else {
+				patched.append(LAUNCHER_LIB_ARG).append("\n");
+				patched.append(
+						String.format("../../../plugins/%s",
+								launcherLib.getName())).append("\n");
+			}
 			patched.append(contents);
 			FileUtils.fileWrite(iniFile, patched.toString());
 
@@ -139,6 +123,26 @@ public class MyMojo extends AbstractMojo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private List<File> getIniFiles(File parent) {
+		List<File> result = new ArrayList<File>();
+		for (File child : parent.listFiles()) {
+			if (child.isFile() && child.getName().endsWith(INI_EXT)) {
+				result.add(child);
+			}
+		}
+		return result;
+	}
+
+	private List<File> getAppDirs(File parent) {
+		List<File> result = new ArrayList<File>();
+		for (File child : parent.listFiles()) {
+			if (child.isDirectory() && child.getName().endsWith(APP_EXT)) {
+				result.add(child);
+			}
+		}
+		return result;
 	}
 
 	/**
@@ -170,7 +174,33 @@ public class MyMojo extends AbstractMojo {
 		return candidates.get(candidates.size() - 1);
 	}
 
-	private static final String LAUNCHER_JAR = "org.eclipse.equinox.launcher";
+	private File findLauncherLib(File macDir) {
+		File plugins = new File(macDir, "plugins");
+		if (!plugins.exists()) {
+			return null;
+		}
+
+		List<File> candidates = new ArrayList<File>();
+		for (File file : plugins.listFiles()) {
+			if (file.isDirectory() && file.getName().startsWith(LAUNCHER_LIB_PREFIX)) {
+				candidates.add(file);
+			}
+		}
+		if (candidates.isEmpty()) {
+			return null;
+		}
+
+		// here should be honest sort by version, but for now assume that
+		// string comparison works fine
+		Collections.sort(candidates);
+		return candidates.get(candidates.size() - 1);
+	}
+
+	private static final String LAUNCHER_JAR = "org.eclipse.equinox.launcher_";
 	private static final String JAR_EXT = ".jar";
+	private static final String APP_EXT = ".app";
+	private static final String INI_EXT = ".ini";
 	private static final String STARTUP_ARG = "-startup";
+	private static final String LAUNCHER_LIB_ARG = "--launcher.library";
+	private static final String LAUNCHER_LIB_PREFIX = "org.eclipse.equinox.launcher.cocoa.macosx";
 }
